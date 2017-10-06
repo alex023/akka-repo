2470563aecb57d2178564eec13aff2244f78ad6a
alex_023 <12242284@qq.com>
2017-10-06 14:03:36 +0800

    refactor agent的内部数据结构调整

--- world/src/main/scala/core/Agent.scala
+++ world/src/main/scala/core/Agent.scala
@@ -1,51 +1,58 @@
 package core
 
+import scala.collection.mutable
+
 //全局的 Agent 容器
 object Agent {
   //所有游戏对象的全局索引，Gid->Agent
-  private var agents = Map.empty[Int, Agent]
+  private var agents = new mutable.HashMap[Long, Agent]()
 
   def load(): Unit = {
     //todo:
     //从数据库中加载
   }
 
-  def getAgent(agentId: Int): Option[Agent] = if (agents contains agentId) {
-    Some(agents(agentId))
-  } else None
+  def getAgent(agentId: Long): Option[Agent] =
+    if (agents contains agentId) {
+      Some(agents(agentId))
+    } else None
 
-  def getAgents(tId: Int): Vector[Agent] = {
+  def getAgents(tId: Long): Vector[Agent] = {
     agents.filter { case (_, agent) => agent.tid == tId }.values.toVector
   }
 
-  def removeAgent(agentId: Int): Unit = {
-    agents = agents.drop(agentId)
+  def removeAgent(agentId: Long): Unit = {
+    agents -= agentId
   }
 
   def addAgent(agent: Agent): Unit = {
-    agents = agents + (agent.id -> agent)
+    agents += (agent.id -> agent)
   }
 }
 
 /*
-游戏单位实现 Buf 效果的单元基础定义，任何需要实现 Buf 效果的子类，都需要继承该类。
-在实现的时候，只需要在重载固定函数的时候，修改自身属性即可
+ * 游戏单位实现 Buf 效果的单元基础定义，任何需要实现 Buf 效果的子类，都需要继承该类。
+ * 在实现的时候，只需要在重载固定函数的时候，修改自身属性即可.
+ * Note:Agent不支持多线程，在应用时需要确保同一时刻只会被一个方法驱动
  */
 trait Agent {
-  val id: Int
+  val id: Long
   val tid: Int
-  private var buffs = Map.empty[Int, Buff]
+  private var buffs = new mutable.HashMap[Int, Buff]()
 
   final def addBuf(buf: Buff): Unit = {
-    buffs = buffs + (buf.id -> buf)
+    buffs += (buf.id -> buf)
     onAddBuf(buf)
   }
 
   final def removeBuf(bufId: Int): Unit = {
     if (buffs contains bufId) {
-      buffs = buffs.drop(bufId)
+      buffs -= bufId
       onRemoveBuf(bufId)
     }
+  }
+  final def currentBuf(): Vector[Buff] = {
+    buffs.values.toVector
   }
 
   protected def onAddBuf(buf: Buff)
